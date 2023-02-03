2b7b296ee4cc8819839bacd908f1d52f5b9c0a80
aumartinez <acm_asd@hotmail.com>
2023-02-03 00:21:42 -0600

    Updated button controller

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,13 +5,24 @@ function Buttons (props) {
   const isOperator = (value) => {
     return isNaN(value) && (value !== '.') && (value !== '=')
   };
-  return (
-    <button className={`btn ${isOperator(props.children) ? 'btn-operator' : ''}`.trimEnd()}
-    onClick={() => props.handleClick(props.children)}
-    >
-      {props.children}
-    </button>
-  );
+
+  if (isOperator(props.children)) {
+    return (
+      <button className='btn btn-operator'
+      onClick={() => props.handleClick(props.children)}
+      >
+        {props.children}
+      </button>
+    );
+  } else {
+    return (
+      <button className='btn'
+      onClick={() => props.handleClick(props.children)}
+      >
+        {props.children}
+      </button>
+    );
+  }
 }
 
 export default Buttons;
